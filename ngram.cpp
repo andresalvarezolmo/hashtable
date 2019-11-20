7610cc871a86176f4f15970204a4c2368a065b34
xDrews <43134259+xDrews@users.noreply.github.com>
2019-11-20 17:04:04 +0000

    parameter 4th working

--- ngram.cpp
+++ ngram.cpp
@@ -25,56 +25,121 @@ HTSI T1(117);
 
 float redondeo(float var) 
 { 
-    // 37.66666 * 100 =3766.66 
-    // 3766.66 + .5 =3767.16    for rounding off value 
-    // then type cast to int so value is 3767 
-    // then divided by 100 so the value converted into 37.67 
-    float value = (int)(var * 100 + .5); 
-    return (float)value / 100; 
+  // 37.66666 * 100 =3766.66 
+  // 3766.66 + .5 =3767.16    for rounding off value 
+  // then type cast to int so value is 3767 
+  // then divided by 100 so the value converted into 37.67 
+  float value = (int)(var * 100 + .5); 
+  return (float)value / 100; 
 } 
 
 // int everything(){
 // int everything(string filename){
-int everything(string filename, string n){
-// int everything(string filename, string n, string k, string keyword){
+// int everything(string filename, string n){
+// int everything(string filename, string n, string k){
+
+int ngram(string filename, string n, string k, string keyword){
 
 // string filename = "a.txt";
 ifstream reader( filename );
 
 if( ! reader ) {
-    cout << "Error opening input file" << endl;
+    cout << "Error opening input file, please enter a valid filename with the extension .txt" << endl;
     return 1;
 }
 string line;
-int ngrams = 0;
-int i = 0;
 char letter;
 
 stringstream middle1(n);
 int length = 0;
 middle1>>length;
-// stringstream middle2(k); 
+stringstream middle2(k); 
+int ngrams = 0;
+middle2>>ngrams;
 
 while(reader.get(letter)) {
   line += letter;
-
 }
+
 //   cout << line[i] << line [i+1] << line[i+2] << endl;
 //   int val = line[i] + line [i+1] + line[i+2];
 //   cout << val << endl;
-int filled = 0;  
-for (int i = 0; i < line.size()-(length-1); i++)
+int filled = 0;
+
+if (keyword == "word")
 {
-  //   int val = 0;
-  //   val = line[i] + line [i+1] + line[i+2];
-  //   cout << "i: " << i << " "<< line [i] << endl;
+  //  // Used to split string around spaces. 
+  //   istringstream ss(line); 
+  
+  //   // Traverse through all words 
+  //   do { 
+  //       // Read a word 
+  //       string word; 
+  //       ss >> word; 
+  
+  //       // Print the read word 
+  //       T1.insert(word, 1);
+  //       filled++;
+  
+  //       // While there is more to read 
+  //   } while (ss); 
+// }     
+  string w = "";
+  int counter = 0;
+  for (int i = 0; i < line.size(); i++) 
+  { 
+      // cout << "i value: " << i<< endl;
+    if (line[i] == ' ' || line[i] == '\n') 
+    { 
+      counter++;
+
+      if (counter == length)
+      {
+        T1.insert(w, 1);
+        filled++;
+        w = "";
+        counter = 0;
+      }
+      
+    } 
+    else
+    { 
+      w = w + line[i]; 
+    } 
+  }  
+  // T1.insert(w, 1);
+  // filled++;
+} 
+    
+else if (keyword == "char" || keyword == "decimal")
+{
+  for (int i = 0; (unsigned)i < line.size()-(length-1); i++)
+  {
     string inside = line.substr(i, length);
-  //   cout << inside << "," << endl;
-  //   cout << "i: " << i << " i+1: "<< i+1 << " i+2: " << i+2 <<endl;
-  //   cout << line.substr(i,i+2) << ","<< endl;
     T1.insert(line.substr(i,length), 1);
     filled++;
+  }
 }
+else
+{
+  cout << "Please enter a valid option for the 4th parameter" <<endl;
+  return 1;
+}
+
+//ESTE ES EL GENERICO, ARRIBA ESTAN LOS ESPECIALIZADOS PARA EL KEYWORD
+// for (int i = 0; (unsigned)i < line.size()-(length-1); i++)
+// {
+//   //   int val = 0;
+//   //   val = line[i] + line [i+1] + line[i+2];
+//   //   cout << "i: " << i << " "<< line [i] << endl;
+//     string inside = line.substr(i, length);
+//   //   cout << inside << "," << endl;
+//   //   cout << "i: " << i << " i+1: "<< i+1 << " i+2: " << i+2 <<endl;
+//   //   cout << line.substr(i,i+2) << ","<< endl;
+//     T1.insert(line.substr(i,length), 1);
+//     filled++;
+// }
+
 //   T1.printTable();
   
 //fill an array with the values of the  hashmap's nodes
@@ -85,13 +150,14 @@ for (int i = 0; i < line.size()-(length-1); i++)
       // cout << T1.getTable()[d].getValue() << endl;        
       values [d] =  T1.getTable()[d].getValue();
   }
-  sort(values, values+si,greater<int>()); 
+  sort(values, values+si,greater<int>());
 
-// for (int i = 0; i < n; ++i) 
+// for (int i = 0; i < si; ++i) 
 //     cout << values[i] << endl;
 
 // T1.printTable();
 
+//PRINTING THE DETAILS
 int times = 0;
 for (int v = 0; v < si; v++)
 {
@@ -111,13 +177,13 @@ for (int v = 0; v < si; v++)
           // cout << "Key: " <<T1.getTable()[q].getKey()<< ",with value: " << T1.getTable()[q].getValue()<<endl;
           times++;
 
-          if(times >= 10)
+          if(times >= ngrams)
             break;
         }
       while (values[v]==values[v+1])
       v++;       
     }
-  if(times >= 10)
+  if(times >= ngrams)
     break;   
 }
   // cout << "valor de filled: "<< filled << endl;
@@ -139,7 +205,8 @@ for (int v = 0; v < si; v++)
 int main(int argc,char* argv[]){
 
   // everything(argv[1]);
-  everything(argv[1], argv[2]);
+  // everything(argv[1], argv[2]);
   // everything(argv[1], argv[2], argv[3]);
+  ngram(argv[1], argv[2], argv[3], argv[4]);
     
 }
